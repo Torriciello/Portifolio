04e94fee8b35a6c828b57faa56b53f8a9ad14bf7
Gabriel Torriciello <gabrielvictortorriciello@outlook.com>
2026-03-05 13:54:18 -0300

    finalizada a tela de login

--- portifolio/frontend/reciclaPro/reciclaPro-project/src/App.jsx
+++ portifolio/frontend/reciclaPro/reciclaPro-project/src/App.jsx
@@ -1,26 +1,20 @@
-import './App.css'
-import { Form_login } from './components/form_login'
+import "./App.css";
+import { Form_login } from "./components/form_login";
 
 function App() {
   return (
     <main className="container">
-
       <div className="left">
-
         <img src="./logo.svg" className="logo" />
 
         <Form_login />
-
       </div>
 
       <div className="right">
-
         <img src="./reciclagem.png" className="image" />
-
       </div>
-
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
